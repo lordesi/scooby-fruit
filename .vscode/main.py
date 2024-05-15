97a3913dd19dead21c761cfd6b4460385d7db394
Gabriele <gabrielefaruzzi@gmail.com>
2024-05-15 22:03:33 +0200

    aggiunta base per schermata iniziale e immagine pulsante angura

--- .vscode/main.py
+++ .vscode/main.py
@@ -2,6 +2,12 @@ import pygame, sys
 import fruit
 import settings
 
+bianco=(255,255,255)
+nero=(0,0,0)
+rosso=(255,0,0)
+blu=(0,0,255)
+verde=(0,255,0)
+
 pygame.init()
 
 #definisco la schermata di avvio - demo
@@ -16,7 +22,7 @@ clock = pygame.time.Clock()
 #sfondo caricamento
 immagine_caricamento=pygame.image.load("schermata-iniziale.png")
 immagine_caricamento=pygame.transform.scale(immagine_caricamento,(settings.WINDOW_WIDTH,settings.WINDOW_HEIGHT))
-#barra di caricament0
+#barra di caricamento
 barra=[]
 frame=pygame.image.load("25.jpeg")
 frame=pygame.transform.scale(frame,(500,40))
@@ -43,23 +49,63 @@ def schermata_caricamento():
         if i==4:
             stato=False
 
+#Rect e immagine per schermata iniziale
+play_rect = pygame.Rect(settings.WINDOW_WIDTH/2 - 55, settings.WINDOW_HEIGHT/2+100 - 25, 110, 110)
+play_rect_schiacciato = pygame.Rect(settings.WINDOW_WIDTH/2 - 64, settings.WINDOW_HEIGHT/2+100 - 25, 128, 128)
+pulsante_gioca=pygame.image.load("anguria pulsante.jpg")
+pulsante_gioca=pygame.transform.scale(pulsante_gioca,(110,110))
+pulsante_gioca_cliccato=pygame.transform.scale(pulsante_gioca,(128,128))
+
+#funzione schermata iniziale
+def schermata_iniziale():
+    inizio=True
+    while inizio:
+        screen.fill(bianco)
+        font=pygame.font.SysFont('arial',40)
+        titolo=font.render('Scooby fruit',True,(0,100,0))
+        screen.blit(titolo, (settings.WINDOW_WIDTH/2 - titolo.get_width()/2, settings.WINDOW_HEIGHT/4 - titolo.get_height()/2))
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                inizio = False
+
+            if event.type==pygame.MOUSEMOTION:
+                posizione=pygame.mouse.get_pos()
+                if play_rect.collidepoint(posizione):
+                    screen.blit(pulsante_gioca_cliccato,play_rect_schiacciato)
+                    #pulsante_gioca=pygame.transform.scale(pulsante_gioca,(128,128))
+                    pygame.display.update()
+            
+            if event.type==pygame.MOUSEMOTION:
+                posizione=pygame.mouse.get_pos()
+                if not play_rect.collidepoint(posizione):
+                    screen.blit(pulsante_gioca,play_rect)
+                    pygame.display.update()
+            
+            if event.type==pygame.MOUSEBUTTONDOWN:
+                posizione=pygame.mouse.get_pos()
+                if play_rect.collidepoint(posizione):
+                    inizio=False
+        
+
 schermata_caricamento()
+schermata_iniziale()
 
 #Loop principale del gioco (eventi) - demo
 
-running = True
+#running = True
 
-while running:
+#while running:
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    #for event in pygame.event.get():
+        #if event.type == pygame.QUIT:
+            #running = False
             
     
 
 
-    pygame.display.flip()
-    clock.tick(settings.FPS)
+    #pygame.display.flip()
+    #clock.tick(settings.FPS)
 
 pygame.quit()
 sys.exit()
